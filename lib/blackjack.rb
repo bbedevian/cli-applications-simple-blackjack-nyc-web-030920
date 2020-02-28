897d1a2432883492db76d0158f982666fabccb62
Brett Bedevian <brettbedevian@gmail.com>
2020-02-28 15:46:47 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,9 +33,8 @@ def initial_round
   card_1 = deal_card
   card_2 = deal_card
   card_total = card_1 + card_2
-  return card_total
   display_card_total(card_total)
-  
+  return card_total
 end
 
 def hit?
