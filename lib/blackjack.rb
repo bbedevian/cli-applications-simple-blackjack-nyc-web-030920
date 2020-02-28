c7a9bad640a6a2df63d572827e0cb612596deb46
Brett Bedevian <brettbedevian@gmail.com>
2020-02-28 15:57:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,11 +40,11 @@ end
 def hit?(card_total)
   # code hit? here
   prompt_user
-  get_user_input
-  if get_user_input == "h"
+  input = get_user_input
+  if input == "h"
     new_card = deal_card
     card_total += new_card
-  elsif get_user_input == "s"
+  elsif input == "s"
   return card_total
   
   else 
