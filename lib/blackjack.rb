959b124167d084976523cbea1f544ebd1512b86a
Brett Bedevian <brettbedevian@gmail.com>
2020-02-28 15:52:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,7 +53,7 @@ end
 
 def invalid_command
   # code invalid_command here
-  print "Please enter a valid comand"
+  puts "Please enter a valid command"
 end
 
 #####################################################
